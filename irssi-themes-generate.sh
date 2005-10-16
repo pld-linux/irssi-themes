47b6f465b29f3c7e451580c7969500939df06fb2
Elan Ruusamäe <glen@pld-linux.org>
2005-10-16 14:17:15 +0000

    - script to make irssi theme snapshot
    Changed files:
        irssi-themes-generate.sh -> 1.1

---
+++ irssi-themes-generate.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+# location for irssi themes
+url='http://www.irssi.org/themes/'
+
+# this script must reside inSOURCES dir
+set -e
+dir=$(dirname "$0")
+cd "$dir"
+cd ../SOURCES # this makes sure we're in right place
+top=$(pwd)
+
+snap=$(date +%Y%m%d)
+if [ ! -f irssi-themes-$snap.tar.bz2 ]; then
+	tmpd=$(mktemp -d ${TMPDIR:-/tmp}/irssiXXXXXX)
+	cd $tmpd
+	mkdir irssi-themes-$snap
+	cd irssi-themes-$snap
+   	wget -r -np -l1 -nH --cut-dirs=1 -A theme $url
+	cd ..
+	tar cjf $top/irssi-themes-$snap.tar.bz2 irssi-themes-$snap
+	cd $top
+fi
